f34bbbe8ecb2fee5d62093c570d6a0060f4eb0be
Chenglong Du <swhite.do@gmail.com>
2026-02-27 15:48:32 +0800

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -14,6 +14,8 @@ cd mtk-openwrt-feeds; git checkout e85f8374bdffb1ebd0d1c0822c5c3992d1201f49; cd 
 
 \cp -r my_files/9999-image-bpi-r4-sdcard.patch mtk-openwrt-feeds/25.12/patches-base
 
+\cp -r my_files/99999-cpu-overclock.patch mtk-openwrt-feeds/25.12/files/target/linux/mediatek/patches-6.12
+
 ### tx_power check Gilly_1970's patch - for defective BE14 boards with defective eeprom flash
 #\cp -r my_files/0140-wifi-mt76-mt7996-use-mt76_get_txpower_cur.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/25.12/files/package/kernel/mt76/patches
 
